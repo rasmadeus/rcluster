638cfccb5fee243c3fe9456f32bab36a91cfb64b
Konstantin Kulikov <rasmadeus@gmail.com>
2018-08-07 17:33:50 +0300

    Fixed sorting

--- src/libs/widgets/slave_sort_model.cpp
+++ src/libs/widgets/slave_sort_model.cpp
@@ -13,7 +13,5 @@ bool SlaveSortModel::lessThan(QModelIndex const &sourceLeft, QModelIndex const &
     auto const leftCaption = sourceLeft.data();
     auto const rightCaption = sourceRight.data();
 
-    return leftType == rightType
-        ? leftCaption.toString().localeAwareCompare(rightCaption.toString())
-        : leftType.toString().localeAwareCompare(rightType.toString());
+    return leftType == rightType ? leftCaption.toString().localeAwareCompare(rightCaption.toString()) : leftType < rightType;
 }
